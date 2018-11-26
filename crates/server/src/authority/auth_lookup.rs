4ae074331d2a1ad3c422ab6eb1f7cad8d87f0fb7
Benjamin Fry <benjaminfry@me.com>
2018-11-25 16:45:45 -0800

    move all Lookup iterators to auth_lookup module

--- crates/server/src/authority/auth_lookup.rs
+++ crates/server/src/authority/auth_lookup.rs
@@ -6,10 +6,12 @@
 // copied, modified, or distributed except according to those terms.
 
 use std::iter::Chain;
+use std::slice::Iter;
+use std::sync::Arc;
 
-use trust_dns::rr::Record;
-
-use store::sqlite::{LookupRecords, LookupRecordsIter};
+use proto::rr::dnssec::SupportedAlgorithms;
+use proto::rr::{Record, RecordSet, RecordType, RrsetRecords};
+use trust_dns::rr::LowerName;
 
 /// The result of a lookup on an Authority
 ///
@@ -137,5 +139,236 @@ impl<'a> Default for AuthLookupIter<'a> {
 impl From<LookupRecords> for AuthLookup {
     fn from(lookup: LookupRecords) -> Self {
         AuthLookup::Records(lookup)
+    }
+}
+
+/// An iterator over an ANY query for Records.
+///
+/// The length of this result cannot be known without consuming the iterator.
+///
+/// # Lifetimes
+///
+/// * `'r` - the record_set's lifetime, from the catalog
+/// * `'q` - the lifetime of the query/request
+#[derive(Debug)]
+pub struct AnyRecords {
+    is_secure: bool,
+    supported_algorithms: SupportedAlgorithms,
+    rrsets: Vec<Arc<RecordSet>>,
+    rrset: Option<Arc<RecordSet>>,
+    records: Option<Arc<RecordSet>>,
+    query_type: RecordType,
+    query_name: LowerName,
+}
+
+impl AnyRecords {
+    /// construct a new lookup of any set of records
+    pub fn new(
+        is_secure: bool,
+        supported_algorithms: SupportedAlgorithms,
+        // TODO: potentially very expensive
+        rrsets: Vec<Arc<RecordSet>>,
+        query_type: RecordType,
+        query_name: LowerName,
+    ) -> Self {
+        AnyRecords {
+            is_secure,
+            supported_algorithms,
+            rrsets,
+            rrset: None,
+            records: None,
+            query_type,
+            query_name,
+        }
+    }
+
+    fn iter(&self) -> AnyRecordsIter {
+        self.into_iter()
+    }
+}
+
+impl<'r> IntoIterator for &'r AnyRecords {
+    type Item = &'r Record;
+    type IntoIter = AnyRecordsIter<'r>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        AnyRecordsIter {
+            is_secure: self.is_secure,
+            supported_algorithms: self.supported_algorithms,
+            // TODO: potentially very expensive
+            rrsets: self.rrsets.iter(),
+            rrset: None,
+            records: None,
+            query_type: self.query_type,
+            query_name: &self.query_name,
+        }
+    }
+}
+
+/// An iteration over a lookup for any Records
+pub struct AnyRecordsIter<'r> {
+    is_secure: bool,
+    supported_algorithms: SupportedAlgorithms,
+    rrsets: Iter<'r, Arc<RecordSet>>,
+    rrset: Option<&'r RecordSet>,
+    records: Option<RrsetRecords<'r>>,
+    query_type: RecordType,
+    query_name: &'r LowerName,
+}
+
+impl<'r> Iterator for AnyRecordsIter<'r> {
+    type Item = &'r Record;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::borrow::Borrow;
+
+        let query_type = self.query_type;
+        let query_name = self.query_name;
+
+        loop {
+            if let Some(ref mut records) = self.records {
+                let record = records
+                    .by_ref()
+                    .filter(|rr_set| {
+                        query_type == RecordType::ANY || rr_set.record_type() != RecordType::SOA
+                    }).find(|rr_set| {
+                        query_type == RecordType::AXFR
+                            || &LowerName::from(rr_set.name()) == query_name
+                    });
+
+                if record.is_some() {
+                    return record;
+                }
+            }
+
+            self.rrset = self.rrsets.next().map(|r| r.borrow());
+
+            // if there are no more RecordSets, then return
+            self.rrset?;
+
+            // getting here, we must have exhausted our records from the rrset
+            self.records = Some(
+                self.rrset
+                    .expect("rrset should not be None at this point")
+                    .records(self.is_secure, self.supported_algorithms),
+            );
+        }
+    }
+}
+
+/// The result of a lookup
+#[derive(Debug)]
+pub enum LookupRecords {
+    /// There is no record by the name
+    NxDomain,
+    /// There is no record for the given query, but there are other records at that name
+    NameExists,
+    /// The associate records
+    Records(bool, SupportedAlgorithms, Arc<RecordSet>),
+    // TODO: need a better option for very large zone xfrs...
+    /// A generic lookup response where anything is desired
+    AnyRecords(AnyRecords),
+}
+
+impl LookupRecords {
+    /// Construct a new LookupRecords
+    pub fn new(
+        is_secure: bool,
+        supported_algorithms: SupportedAlgorithms,
+        records: Arc<RecordSet>,
+    ) -> Self {
+        LookupRecords::Records(is_secure, supported_algorithms, records)
+    }
+
+    /// This is an NxDomain or NameExists, and has no associated records
+    ///
+    /// this consumes the iterator, and verifies it is empty
+    pub fn was_empty(&self) -> bool {
+        self.iter().count() == 0
+    }
+
+    /// This is an NxDomain
+    pub fn is_nx_domain(&self) -> bool {
+        match *self {
+            LookupRecords::NxDomain => true,
+            _ => false,
+        }
+    }
+
+    /// This is a NameExists
+    pub fn is_name_exists(&self) -> bool {
+        match *self {
+            LookupRecords::NameExists => true,
+            _ => false,
+        }
+    }
+
+    /// Conversion to an iterator
+    pub fn iter(&self) -> LookupRecordsIter {
+        self.into_iter()
+    }
+}
+
+impl Default for LookupRecords {
+    fn default() -> Self {
+        LookupRecords::NxDomain
+    }
+}
+
+impl<'a> IntoIterator for &'a LookupRecords {
+    type Item = &'a Record;
+    type IntoIter = LookupRecordsIter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        match self {
+            LookupRecords::NxDomain | LookupRecords::NameExists => LookupRecordsIter::Empty,
+            LookupRecords::Records(is_secure, supported_algorithms, r) => {
+                LookupRecordsIter::RecordsIter(r.records(*is_secure, *supported_algorithms))
+            }
+            LookupRecords::AnyRecords(r) => LookupRecordsIter::AnyRecordsIter(r.iter()),
+        }
+    }
+}
+
+/// Iteratof over lookup records
+pub enum LookupRecordsIter<'r> {
+    /// An iteration over batch record type results
+    AnyRecordsIter(AnyRecordsIter<'r>),
+    /// An iteration over a single RecordSet
+    RecordsIter(RrsetRecords<'r>),
+    /// An empty set
+    Empty,
+}
+
+impl<'r> Default for LookupRecordsIter<'r> {
+    fn default() -> Self {
+        LookupRecordsIter::Empty
+    }
+}
+
+impl<'r> Iterator for LookupRecordsIter<'r> {
+    type Item = &'r Record;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            LookupRecordsIter::Empty => None,
+            LookupRecordsIter::AnyRecordsIter(current) => current.next(),
+            LookupRecordsIter::RecordsIter(current) => current.next(),
+        }
+    }
+}
+
+// impl From<Arc<RecordSet>> for LookupRecords {
+//     fn from(rrset_records: Arc<RecordSet>) -> Self {
+//         match *rrset_records {
+//             RrsetRecords::Empty => LookupRecords::NxDomain,
+//             rrset_records => LookupRecords::RecordsIter(rrset_records),
+//         }
+//     }
+// }
+
+impl From<AnyRecords> for LookupRecords {
+    fn from(rrset_records: AnyRecords) -> Self {
+        LookupRecords::AnyRecords(rrset_records)
     }
 }
